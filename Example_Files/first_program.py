4fd3bbcac438d40aab8ba7d781e6862c5aa55e46
Chris Vintzileos <chris.vintzileos@gmail.com>
2023-04-05 17:41:35 +0000

    jk

---
+++ Example_Files/first_program.py
@@ -0,0 +1,21 @@
+greet = "Hello how are you"
+Extended_greet = "Hello how are you, " + "long string"
+
+single = print(input("Are you married?"))
+
+
+
+name = "Chris"
+intruption = f"Hello {name}"
+greet_format = "Hello {}"
+
+if (single == "yes" or "Yes"):
+    print(name + " nice you're not single")
+else:
+    print(name + " nice you're single")
+
+
+
+print(intruption, Extended_greet)
+print(name.upper())
+print(name.lower())
