47858a9e1e08982c6dbd673fc41bcdff71b491bf
Chris Vintzileos <chris.vintzileos@gmail.com>
2023-04-06 14:35:21 +0000

    Created file

---
+++ Example_Files/List_example.py
@@ -0,0 +1,15 @@
+Names = ["John", "Paul", "George", "Ringo"]
+Ages = [21, 22, 23, 30]
+John = Names[0]
+Paul = Names[1]
+First_two = Names[:2]
+Last_two = Names[2:]
+Every_other = Names[::-1]
+
+print(sum(Ages))
+print(min(Ages))
+print(max(Ages))
+
+print(First_two)
+print(Last_two)
+print(Every_other)
--- Example_Files/first_program.py
+++ Example_Files/first_program.py
@@ -4,16 +4,16 @@ Extended_greet = "Hello how are you, " + "long string"
 single = print(input("Are you married?"))
 
 
-
 name = "Chris"
 intruption = f"Hello {name}"
 greet_format = "Hello {}"
 
 if (single == "yes" or "Yes"):
     print(name + " nice you're not single")
-else if(single == "no" or "No"):
+elif (single == "no" or "No"):
     print(name + " nice you're single")
-
+else:
+    print("Fine, don't give me an answer")
 
 
 print(intruption, Extended_greet)
